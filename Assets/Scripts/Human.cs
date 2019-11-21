26edccd017dd2d35e2796e874f051fe7dd4ec164
TKGameCreate <tokisada.tnw@gmail.com>
2019-11-21 21:46:52 +0900

    PlayerのAnimationContotrollerの追加 Animation処理の追加 Jump処理の追加 Jump処理終了のアニメーションイベントの追加 UpdateをHuman単体に付与 仮置きで高木テクスチャの追加

--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -5,12 +5,45 @@ using UnityEngine;
 public class Human : Player
 {
     [SerializeField] private Rigidbody rbody;
-    [SerializeField] private Transform controlObject;
     [SerializeField] private float speed = 5.0f;
     [SerializeField] private float rotSpeed = 10.0f;
+    [SerializeField] private float jumpPower = 1.5f;
+    [SerializeField] private Animator anim;
 
-    private float horizontal;
-    private float vertical;
+    private Vector3 velocity;
+    private float horizontal = 0;
+    private float vertical = 0;
+    private bool jump = false;
+
+    private void Update()
+    {
+        AnimationControl();
+        Operation();
+    }
+
+    private void AnimationControl()
+    {
+        //移動アニメーション
+        if (velocity.magnitude > 0.1f)
+        {
+            anim.SetFloat("speed", velocity.magnitude);
+        }
+        else
+        {
+            anim.SetFloat("speed", 0);
+        }
+        //ジャンプアニメーション
+        if (jump)
+        {
+             anim.SetBool("jump", true);
+        }
+        else
+        {
+            anim.SetBool("jump", false);
+        }
+
+        //マシンに乗るアニメーション
+    }
 
     protected override void Operation()
     {
@@ -20,8 +53,11 @@ public class Human : Player
         horizontal = InputManager.Instance.InputLeftStick(true);
         vertical = InputManager.Instance.InputLeftStick(false);
 
+        //移動量
+        velocity = new Vector3(horizontal * speed, 0, vertical * speed);
+
         //アナログスティックのグラつきを想定して±0.01以下をはじく
-        if (Mathf.Abs(horizontal) + Mathf.Abs(vertical) > 0.1F)
+        if (Mathf.Abs(horizontal) + Mathf.Abs(vertical) > 0.1f)
         {
             //カメラからみたプレイヤーの方向ベクトル
             Vector3 camToPlayer = transform.position - Camera.
@@ -33,10 +69,22 @@ public class Human : Player
             //deltaTimeを用いることで常に一定の速度になる
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotSpeed);
         }
+
+        if (InputManager.Instance.InputAButtonDown())
+        {
+            jump = true;
+            rbody.AddForce(Vector3.up * jumpPower);
+        }
     }
 
     private void FixedUpdate()
     {
-        rbody.velocity = new Vector3(horizontal, 0, vertical);
+        rbody.velocity = velocity;
+    }
+
+    //AnimationEvent
+    public void JumpEndAnimation()
+    {
+        jump = false;
     }
 }
