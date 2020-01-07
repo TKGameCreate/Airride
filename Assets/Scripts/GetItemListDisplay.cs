bed27bb008247fa959ba13a51025bce5c33c17b0
tokisada_tnw <tokisada.tnw@gmail.com>
2020-01-08 00:49:50 +0900

    制限時間を元に

--- Assets/Scripts/GetItemListDisplay.cs
+++ Assets/Scripts/GetItemListDisplay.cs
@@ -16,7 +16,7 @@ public class GetItemListDisplay : MonoBehaviour
         }
 
         //画像のセット
-        Machine machine = player.LastRideMachine;
+        Machine machine = player.Machine;
         for (int i = 0; i < numText.Length; i++)
         {
             numText[i].text = machine.GetItemTotal(i).ToString();
