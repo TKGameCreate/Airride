dc740534098162e606c1c10a5884e6356a98dc7a
tokisada_tnw <tokisada.tnw@gmail.com>
2020-01-12 00:20:00 +0900

    Title画面の仮作成

---
+++ Assets/Scripts/Temps/TestLoad.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// テスト用Sceneチェンジスクリプト
+/// </summary>
+public class TestLoad : MonoBehaviour
+{
+    public void InputButtonSceneChange(int no)
+    {
+        AirrideScene scene = (AirrideScene)no;
+        AirrideSceneManager.Instance.LoadScene(scene);
+    }
+}
