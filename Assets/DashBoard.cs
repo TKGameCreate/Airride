4c3a2170136520f22ca3038b13d4ee0edc672969
TKGameCreate <tokisada.tnw@gmail.com>
2020-01-30 14:54:08 +0900

    DashBoard.csの作成

---
+++ Assets/DashBoard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashBoard : MonoBehaviour
+{
+    private const float dashBoardUpSpeed = 125.0f; //ダッシュボード倍率
+    private bool coolDown = false;
+
+    public float Dash()
+    {
+        if (!coolDown)
+        {
+            StartCoroutine(DashCoolDown());
+            return dashBoardUpSpeed;
+        }
+        return 0;
+    }
+
+    private IEnumerator DashCoolDown()
+    {
+        coolDown = true;
+        float interval = 10.0f;
+        yield return new WaitForSeconds(interval);
+        coolDown = false;
+    }
+}
